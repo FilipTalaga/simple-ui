02b07ac28dd67d57a8af5924043efb425ddab664
Filip Talaga <talaga.filip@gmail.com>
2022-05-01 20:19:16 +0200

    Introduce publishing to npm repository

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.ts',
   output: {
-    filename: 'bundle.js',
+    filename: 'lib.js',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'production',
